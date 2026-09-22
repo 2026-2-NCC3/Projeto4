a6c8d2c6914c5f311dad4bd398b177ce37bb5021
FabrizzioPuttini <fabrizzioputtini@gmail.com>
2026-09-22 11:09:47 -0300

    Tela inicial finalizada

--- src/Entrega 1/Frontend/app/src/main/java/com/example/myapplication/MainActivity.java
+++ src/Entrega 1/Frontend/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,18 +14,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnLogin, btnCadastro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCadastro = findViewById(R.id.btnCadastro);
 
-        TextView textLogin = findViewById(R.id.textLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(telaLogin);
+            }
+        });
 
-        textLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaCadastro = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(telaCadastro);
+            }
         });
     }
 }
