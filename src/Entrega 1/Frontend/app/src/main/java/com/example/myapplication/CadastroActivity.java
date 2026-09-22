4e2819c0d6d86a000791ae7f605e3d276cfd9c08
FabrizzioPuttini <fabrizzioputtini@gmail.com>
2026-09-22 12:10:49 -0300

    Ajustes nas telas de Login e Cadastro e Alternancia entre telas sendo realizada

--- src/Entrega 1/Frontend/app/src/main/java/com/example/myapplication/CadastroActivity.java
+++ src/Entrega 1/Frontend/app/src/main/java/com/example/myapplication/CadastroActivity.java
@@ -1,4 +1,38 @@
 package com.example.myapplication;
 
-public class CadastroActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    ImageButton btnVoltar;
+    Button btnCadastro, btnLogin;
+    EditText editNome, editEmail, editCelular, editSenha;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        btnVoltar = findViewById(R.id.btnVoltar);
+        btnCadastro = findViewById(R.id.btnCadastro);
+        btnLogin = findViewById(R.id.btnLogin);
+        editNome = findViewById(R.id.editNome);
+        editEmail = findViewById(R.id.editEmail);
+        editCelular = findViewById(R.id.editCelular);
+        editSenha = findViewById(R.id.editSenha);
+
+        btnVoltar.setOnClickListener(view -> {
+            Intent voltar = new Intent(CadastroActivity.this, MainActivity.class);;
+            startActivity(voltar);
+        });
+        btnLogin.setOnClickListener(v -> {
+            Intent login = new Intent(CadastroActivity.this, LoginActivity.class);
+            startActivity(login);
+        });
+    }
 }
--- src/Entrega 1/Frontend/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ src/Entrega 1/Frontend/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,22 +2,36 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ImageButton btnVoltar;
+    Button btnLogin, btnCadastro;
+    EditText LoginEmail, LoginSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView textCadastro = findViewById(R.id.textCadastro);
+        btnVoltar = findViewById(R.id.btnVoltar);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCadastro = findViewById(R.id.btnCadastro);
+        LoginEmail = findViewById(R.id.LoginEmail);
+        LoginSenha = findViewById(R.id.LoginSenha);
 
-        textCadastro.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
+        btnVoltar.setOnClickListener(v -> {
+            Intent voltar = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(voltar);
+        });
+        btnCadastro.setOnClickListener(v -> {
+            Intent cadastro = new Intent(LoginActivity.this, CadastroActivity.class);
+            startActivity(cadastro);
         });
     }
 }
