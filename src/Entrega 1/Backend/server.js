a0cf381b3f6507ecbf9040a28d94d615e40b44f5
FabrizzioPuttini <fabrizzioputtini@gmail.com>
2026-09-24 13:04:23 -0300

    Reestruturação do repositorio

--- src/Entrega 1/Backend/server.js
+++ src/Entrega 1/Backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/',(req, res)=>{
-    res.send('Backend funcionand');
+    res.send('Backend funcionando');
 });
 
 app.post('/cadastro',(req, res)=>{
--- src/Entrega 1/Frontend/app/src/main/java/com/example/myapplication/PerfilActivity.java
+++ src/Entrega 1/Frontend/app/src/main/java/com/example/myapplication/PerfilActivity.java
@@ -1,4 +1,28 @@
 package com.example.myapplication;
 
-public class PerfilActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PerfilActivity extends AppCompatActivity {
+
+    ImageView avatarPerfil;
+    TextView textNome, textEscola, btnSair;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+
+        avatarPerfil = findViewById(R.id.avatarPerfil);
+        textNome = findViewById(R.id.textNome);
+        textEscola = findViewById(R.id.textEscola);
+        btnSair = findViewById(R.id.btnSair);
+
+
+    }
+
 }
+
