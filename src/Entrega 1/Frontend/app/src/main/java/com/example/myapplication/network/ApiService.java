3928d1f3e9904694de35df506e94709f24dc4ec2
FabrizzioPuttini <fabrizzioputtini@gmail.com>
2026-09-25 11:34:56 -0300

    Integração entre o front e o back end das páginas de login e cadastro

--- src/Entrega 1/Frontend/app/src/main/java/com/example/myapplication/network/ApiService.java
+++ src/Entrega 1/Frontend/app/src/main/java/com/example/myapplication/network/ApiService.java
@@ -5,8 +5,10 @@ import com.example.myapplication.AgendaItem;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 /**
@@ -32,4 +34,9 @@ public interface ApiService {
     Call<PerfilResponse> getPerfil(
             @Header("Authorization") String authorization
     );
+    @POST("api/auth/login")
+    Call<LoginResponse> login(@Body LoginRequest loginRequest);
+
+    @POST("api/auth/cadastrar")
+    Call<CadastroResponse> cadastrar(@Body CadastroRequest cadastroRequest);
 }
